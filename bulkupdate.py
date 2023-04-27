3a47932375ba3d265ad8c654755694d75bafab38
Henry Griffiths <42880130+henrygriffiths@users.noreply.github.com>
2023-04-27 11:29:06 -0500

    chore: remove pr assignment (#78)

--- bulkupdate.py
+++ bulkupdate.py
@@ -102,7 +102,7 @@ def main():
         else:
             run(['git', 'push'])
         if config['createpr'] == True:
-            proptions = ['--title', config['msg'], '--body', '{}\n\nCreated by henrygriffiths/bulk-update'.format(config['pr_info']['description']), '-H', dest_branch, '-B', source_branch, '-R', repository, '-a', '@me',]
+            proptions = ['--title', config['msg'], '--body', '{}\n\nCreated by henrygriffiths/bulk-update'.format(config['pr_info']['description']), '-H', dest_branch, '-B', source_branch, '-R', repository]
             if config['pr_info']['merge'] == 'draft':
                 proptions += ['--draft']
             prnum = run(['gh', 'pr', 'create'] + proptions, returnoutput = True)
